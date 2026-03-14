ef7f791b598c3a2f945312dcb2bcee5a730293c6
Vova Polyakov <vova_polyakov_94@mail.rugit config --global user.email>
2026-03-14 23:38:12 +0300

    fix structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
-import "./style.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import bridge from "@vkontakte/vk-bridge";
+
+bridge.send("VKWebAppInit");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+    <App />
+);
