59f3d83bf6b8fe23fd9bc38f8adf9739bfaae882
Vova Polyakov <vova_polyakov_94@mail.rugit config --global user.email>
2026-03-15 00:18:38 +0300

    fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import bridge from "@vkontakte/vk-bridge";
 
-if (window.location.search.includes("vk_platform")) {
-    bridge.send("VKWebAppInit");
-}
+bridge.send("VKWebAppInit");
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
