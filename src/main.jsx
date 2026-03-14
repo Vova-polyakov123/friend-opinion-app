acd03ca92e66c25f0f3e51cd358e72562f8f451d
Vova Polyakov <vova_polyakov_94@mail.rugit config --global user.email>
2026-03-15 02:35:23 +0300

    fix white screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import bridge from "@vkontakte/vk-bridge";
 
-async function start() {
-
-    try {
-        await bridge.send("VKWebAppInit");
-    } catch (e) {
-        console.log("VK init error", e);
-    }
-
-    ReactDOM.createRoot(document.getElementById("root")).render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    );
-
-}
-
-start();
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
